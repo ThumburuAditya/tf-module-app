032d63c6862597405ce40f3d29a8500520ee5c58
ThumburuAditya <chandramouliadityat@gmail.com>
2023-08-27 23:31:03 +0530

    Adding the code

--- vars.tf
+++ vars.tf
@@ -1,2 +1,10 @@
 variable "instance_type" {}
 variable "subnet_ids" {}
+variable "name" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
+variable "bastian_cidr" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
